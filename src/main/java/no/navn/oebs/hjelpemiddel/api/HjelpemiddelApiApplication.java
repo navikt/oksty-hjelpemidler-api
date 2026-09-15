076a2795ce63e1b4eef78ca9bcd9a317ac78931d
Agnes Oftung Strandbu <agnes.oftung.strandbu@nav.no>
2026-09-15 11:03:34 +0200

    chore: deploy t1 instance

---
+++ src/main/java/no/navn/oebs/hjelpemiddel/api/HjelpemiddelApiApplication.java
@@ -0,0 +1,13 @@
+package no.navn.oebs.hjelpemiddel.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjelpemiddelApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HjelpemiddelApiApplication.class, args);
+	}
+
+}
