4fc818475b85339d805d7d406782824ad3be2241
agnesstrandbu-nav <agnes.oftung.strandbu@nav.no>
2026-09-15 11:23:27 +0200

    chore: deploy t1 instance (#1)
    * chore: deploy t1 instance
    
    * fix: correct deploy file
    
    * fix: build without sonar scanning
    
    * fix: correct java version
    
    * fix: use java 21
    
    * fix: add test resources
    
    * cleanup: remove dependabot-deploy

---
+++ src/test/java/no/navn/oebs/hjelpemiddel/api/HjelpemiddelApiApplicationTests.java
@@ -0,0 +1,13 @@
+package no.navn.oebs.hjelpemiddel.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjelpemiddelApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
