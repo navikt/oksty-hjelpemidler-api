2eae85dd63c032300cf7e2510e113b08e75a2794
Agnes Oftung Strandbu <agnes.oftung.strandbu@nav.no>
2026-09-17 08:34:28 +0200

    fix(OEBS-1453): azuread autentication

--- src/main/java/no/navn/oebs/hjelpemiddel/api/HjelpemiddelApiApplication.java
+++ src/main/java/no/navn/oebs/hjelpemiddel/api/HjelpemiddelApiApplication.java
@@ -1,9 +1,14 @@
 package no.navn.oebs.hjelpemiddel.api;
 
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import no.nav.security.token.support.spring.api.EnableJwtTokenValidation;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableJwtTokenValidation(ignore = {"org.springdoc", "org.springframework"})
+@SecurityScheme(type = SecuritySchemeType.HTTP, scheme = "bearer", bearerFormat = "JWT", name = "BearerAuth")
 public class HjelpemiddelApiApplication {
 
 	public static void main(String[] args) {
